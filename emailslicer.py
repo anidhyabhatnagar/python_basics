32963b7e2bf932b22dc787dd61dcccc8fe94823d
anidhyabhatnagar <anidhya@gmail.com>
2019-01-16 20:39:53 +0530

    Added email slicer program skeleton and test.

---
+++ emailslicer.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Anidhya Bhatnagar
+@description: This class provides the functionality required for the
+              email slicer program.
+"""
+
+
+class EmailSlicer:
+    def __init__(self, email=''):
+        pass
+
+    def setEmail(self, email):
+        pass
+
+    def getEmail(self):
+        pass
+
+    def getDomain(self):
+        pass
+
+    def getUser(self):
+        pass
