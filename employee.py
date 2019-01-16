a12c8d4faa323fb376a4203891dfa05f6cb1c399
anidhyabhatnagar <anidhya@gmail.com>
2019-01-16 21:39:35 +0530

    Added employee class skeleton and test.

---
+++ employee.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Anidhya Bhatnagar
+@description: This is the employee class created as per the question for the
+              Classes Exercise Question.
+@question: Create a class called Employee that includes three instance
+           variables — a firstname (type String), a lastname (type String) and
+           a monthly salary (float). Provide a constructor that initializes
+           the three instance variables. Provide set and get methods for
+           each instance variable. If the monthly salary is not positive, do
+           not set its value. Create a method raiseSalary which raises the
+           salary of the employee by 10%. Also create a method getAnnualSalary
+           which returns the employee's annual salary.
+
+           Write a test app named EmployeeTest that demonstrates class
+           Employee’s capabilities.
+
+           Create two Employee objects and display each object’s yearly salary.
+           Then give each Employee a 10% raise and display each Employee’s
+           yearly salary again.
+"""
+
+class Employee:
+    def __init__(self):
+        pass
+
+    def setFirstName(self, firstname):
+        pass
+
+    def setLastName(self, lastname):
+        pass
+
+    def setSalary(self, salary):
+        pass
+
+    def getFirstName(self):
+        pass
+
+    def getLastName(self):
+        pass
+
+    def getSalary(self):
+        pass
+
+    def getAnnualSalary(self):
+        pass
+
+    def raiseSalary(self):
+        pass
