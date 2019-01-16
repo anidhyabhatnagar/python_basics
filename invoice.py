69377aeaa2dc45444fc1bab6b77e4909d6fd945b
anidhyabhatnagar <anidhya@gmail.com>
2019-01-16 23:53:13 +0530

    Added invoice class skeleton and test.

---
+++ invoice.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Anidhya Bhatnagar
+@description: This is the Invoice class created as per the question for the
+              Classes Exercise Question.
+@question: Create a class called Invoice that a hardware store might use to
+           represent an invoice for an item sold at the store.
+
+           An Invoice should include four pieces of information as instance
+           variables—a partNumber, a partDescription, a quantity of the item
+           being purchased and a price per item.
+
+           Your class should have a constructor that initializes the four
+           instance variables. Provide set and get methods for each instance
+           variable.
+
+           In addition, provide a method named getInvoiceAmount that calculates
+           the invoice amount (i.e., multiplies the quantity by the price per
+           item), then returns the amount as a float value.
+             > If the quantity is not positive, it should be set to 0.
+             > If the price per item is not positive, it should be set to 0.
+           Write a test app named InvoiceTest that demonstrates class Invoice’s
+           capabilities.
+"""
+
+class Invoice:
+    def __init__(self):
+        pass
+
+    def setPartNumber(self, partNumber):
+        pass
+
+    def setPartDescription(self, partDescription):
+        pass
+
+    def setQuantity(self, quantity):
+        pass
+
+    def setPrice(self, price):
+        pass
+
+    def getPartNumber(self):
+        pass
+
+    def getPartDescription(self):
+        pass
+
+    def getQuantity(self):
+        pass
+
+    def getPrice(self):
+        pass
+
+    def getInvoiceAmount(self):
+        pass
